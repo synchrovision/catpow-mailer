75e2dbcde85f5f4d32c1de2f56d11cbb8b88cdb7
synchrovision <trinitybluster@yahoo.co.jp>
2021-11-17 17:03:15 +0900

    feat :+1:  implement select_json input type

--- classes/Catpow/input/select_json.php
+++ classes/Catpow/input/select_json.php
@@ -2,11 +2,11 @@
 namespace Catpow\input;
 
 class select_json extends select{
-	public static function get_selections($meta){
-		$rtn=\Catpow\MailForm::get_json($meta->conf['value']??$meta->conf['name']);
-		if(isset($meta->conf['addition'])){
-			if(is_array($meta->conf['addition'])){$rtn=array_merge($rtn,$meta->conf['addition']);}
-			else{$rtn[$meta->conf['addition']]=0;}
+	public static function get_selections($input){
+		$rtn=\Catpow\MailForm::get_json($input->conf['value']??$input->conf['name']);
+		if(isset($input->conf['addition'])){
+			if(is_array($input->conf['addition'])){$rtn=array_merge($rtn,$input->conf['addition']);}
+			else{$rtn[$input->conf['addition']]=0;}
 		}
 		return $rtn;
 	}
