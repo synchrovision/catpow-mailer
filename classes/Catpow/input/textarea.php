f4d0d8171d807fb958e15fb39e1fd8b194f75674
synchrovision <trinitybluster@yahoo.co.jp>
2021-11-17 17:03:24 +0900

    feat :+1:  implement textarea input type

--- classes/Catpow/input/textarea.php
+++ classes/Catpow/input/textarea.php
@@ -3,20 +3,16 @@ namespace Catpow\input;
 
 class textarea extends input{
 	public static
-		$validation=['text'];
+		$validation=array('text'),
+		$default_attr=array('placeholder'=>null,'rows'=>null,'cols'=>null,'autocomplete'=>null,'pattern'=>null);
 	
-	public static function output($meta,$prm){
-		$val=$meta->value;
-		return implode('<br/>',explode(chr(10),$val));
+	public function output(){
+		return nl2br($this->value);
 	}
-	public static function input($meta,$prm){
-		$path=$meta->the_data_path;
-		$val=$meta->value;
+	public function input(){
 		return sprintf(
 			'<textarea name="%s"%s>%s</textarea>',
-			\cp::get_input_name($meta->the_data_path),
-			\cp::get_input_attr($meta->the_data_path,$meta->conf),
-			$meta->value
+			$this->name,$this->attr,$this->value
 		);
 	}
 }
