5c4f217bf5c2be3e6748397791da17695a0a4e47
synchrovision <trinitybluster@yahoo.co.jp>
2021-11-17 17:01:35 +0900

    feat :+1: update input class

--- classes/Catpow/input/input.php
+++ classes/Catpow/input/input.php
@@ -4,7 +4,8 @@ abstract class input{
 	public static
 		$input_type=null,
 		$output_type=null,
-		$validation=array();
+		$validation=array(),
+		$default_attr=array('placeholder'=>null,'size'=>null,'maxlength'=>null,'autocomplete'=>null,'min'=>null,'max'=>null,'step'=>null,'pattern'=>null);
 	public $name,$conf,$form;
 	
 	public function __construct($name,$conf,$form){
@@ -16,26 +17,17 @@ abstract class input{
 		return $this->value;
 	}
 	public function input(){
-		$attr='';
-		foreach(array('placeholder','size','rows','cols','maxlength','autocomplete','min','max','step','pattern') as $attr_name){
-			if(isset($this->conf[$attr_name]))$attr.=' '.$attr_name.'="'.$this->conf[$attr_name].'"';
-		}
-		if(!isset($this->conf['placeholder']))$attr.=' placeholder="'.(isset($this->conf['label'])?$this->conf['label']:'').'"';
-		if(isset($this->conf['attr'])){
-			foreach($this->conf['attr'] as $attr_name=>$attr_val){
-				$attr.=' '.$attr_name.'="'.(is_array($attr_val)?implode(' ',$attr_val):$attr_val).'"';
-			}
-		}
-		if($this->required){$attr.=' required';}
 		return sprintf(
 			'<input type="%s" name="%s" value="%s"%s/>',
 			isset(static::$input_type)?static::$input_type:'text',
-			$this->name,$this->value,$attr
+			$this->name,$this->value,$this->attr
 		);
 	}
 	
 	public function __get($name){
 		switch($name){
+			case 'type':return isset(static::$input_type)?static::$input_type:$this->conf['type'];
+			case 'label':return isset($this->conf['label'])?$this->conf['label']:$this->name;
 			case 'received':return isset($this->form->reserved[$this->name])?$this->form->reserved[$this->name]:'';
 			case 'value':
 				if(isset($this->form->reserved[$this->name]))return $this->form->reserved[$this->name];
@@ -43,6 +35,21 @@ abstract class input{
 				return '';
 			case 'required':return !empty($this->conf['required']) || substr($this->conf['label'],-1)==='*';
 			case 'validation':return isset($this->conf['validation'])?$this->conf['validation']:static::$validation;
+			case 'attr':{
+				$attr='';
+				foreach(array_merge(static::$default_attr,array_intersect_key($this->conf,static::$default_attr)) as $attr_name=>$value){
+					if($value){$attr.=' '.$attr_name.'="'.$value.'"';}
+				}
+				if(!isset($this->conf['placeholder']))$attr.=' placeholder="'.(isset($this->conf['label'])?$this->conf['label']:'').'"';
+				if(isset($this->conf['attr'])){
+					foreach($this->conf['attr'] as $attr_name=>$attr_val){
+						$attr.=' '.$attr_name.'="'.(is_array($attr_val)?implode(' ',$attr_val):$attr_val).'"';
+					}
+				}
+				if($this->required){$attr.=' required';}
+				return $attr;
+			}
+				
 		}
 		if(isset($this->conf[$name])){return $this->conf[$name];}
 	}
