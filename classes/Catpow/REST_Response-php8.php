80acec269ff53d47911f460058bbb23785574fff
synchrovision <trinitybluster@yahoo.co.jp>
2022-09-30 11:28:34 +0900

    feat :+1: php8 compatible

--- classes/Catpow/REST_Response-php8.php
+++ classes/Catpow/REST_Response-php8.php
@@ -1,9 +1,5 @@
 <?php
 namespace Catpow;
-if(\PHP_VERSION_ID>=8000){
-	include __DIR__.'/REST_Response-php8.php';
-	return;
-}
 class REST_Response implements \ArrayAccess{
 	public $status,$data=[];
 	public function __construct(){
@@ -11,16 +7,14 @@ class REST_Response implements \ArrayAccess{
 	public function __toString(){
 		return json_encode($this->data,0500);
 	}
-	
-	public function offsetSet($offset,$value){
+
+	public function offsetSet($offset,$value):void{
 		if(empty($offset) || is_numeric($offset)){return;}
 		$this->data[$offset]=$value;
 	}
-	public function offsetExists($offset){return isset($this->data[$offset]);}
-	public function offsetUnset($offset){unset($this->data[$offset]);}
-	public function offsetGet($offset){
+	public function offsetExists($offset):bool{return isset($this->data[$offset]);}
+	public function offsetUnset($offset):void{unset($this->data[$offset]);}
+	public function offsetGet($offset):mixed{
 		if(isset($this->data[$offset])){return $this->data[$offset];}
 	}
 }
-
-?>
