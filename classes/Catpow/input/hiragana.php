3421ec34decdd4985e6664abb787331d021e2da8
synchrovision <trinitybluster@yahoo.co.jp>
2022-10-05 12:49:06 +0900

    feat :+1: enable to reflect input

--- classes/Catpow/input/hiragana.php
+++ classes/Catpow/input/hiragana.php
@@ -2,6 +2,16 @@
 namespace Catpow\input;
 
 class hiragana extends text{
+	public static $useScripts=array('kana_reflection');
 	static $validation=array('hiragana');
+	
+	public function input(){
+		return sprintf(
+			'<input class="%s" type="%s" name="%s" value="%s"%s%s/>',
+			$this->className,isset(static::$input_type)?static::$input_type:'text',
+			$this->name,$this->value,$this->attr,
+			empty($this->conf['reflect'])?'':' data-hiragana-reflection="'.$this->conf['reflect'].'"'
+		);
+	}
 }
 ?>
