7edffe918c1bfe02758baed740695975e2ba8ef9
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-05-03 19:00:01 +0900

    feat :+1: add filesize validation

---
+++ classes/Catpow/validation/filesize.php
@@ -0,0 +1,24 @@
+<?php
+namespace Catpow\validation;
+
+class filesize extends validation{
+	public static function is_valid(&$val,$input){
+		if(isset($input->conf['filesize'])){
+			if($val['size']>static::filesize_string_to_int($input->conf['filesize'])){
+				return false;
+			}
+		}
+		return true;
+	}
+	public static function get_message_format($conf){
+		return sprintf(__('ファイルサイズは%s以下にしてください'),$conf['filesize']);
+	}
+	private static function filesize_string_to_int($size){
+		if(preg_match('/(\d[\d,]*(?:\.\d+)?)([KMG])B/i',$size,$matches)){
+			return (int)($matches[1]*['K'=>2<<10,'M'=>2<<20,'G'=>2<<30][$matches[2]]);
+		}
+		return (int)$size;
+	}
+}
+
+?>
