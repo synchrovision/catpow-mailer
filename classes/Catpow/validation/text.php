2bc86b2081688f7941d212e30f591fa56a2cc53f
synchrovision <hatano@catpow.info>
2023-05-24 12:39:51 +0900

    feat :+1: unify CRLF to LF

--- classes/Catpow/validation/text.php
+++ classes/Catpow/validation/text.php
@@ -3,7 +3,7 @@ namespace Catpow\validation;
 
 class text extends validation{
 	public static function is_valid(&$val,$input){
-		$val=mb_convert_kana(htmlspecialchars($val),'aKV');
+		$val=mb_convert_kana(htmlspecialchars(preg_replace("/(\r\n|\r)/","\n",$val)),'aKV');
 		return true;
 	}
 	public static function get_message_format($conf){
