a287be799492a4ab3fbff3150c3997b5e143019c
synchrovision <hatano@catpow.info>
2023-05-30 22:19:02 +0900

    fix :bug: do not update value while composing

--- ui/ZipCode/component.js
+++ ui/ZipCode/component.js
@@ -1,14 +1,16 @@
 (() => {
   // ../../mailform/mailer/ui/ZipCode/component.jsx
   Catpow.UI.ZipCode = (props) => {
-    const { useCallback, useState, useRef, useEffect } = React;
+    const { useCallback, useMemo, useState, useRef, useEffect } = React;
     const { className = "cmf-ui-zipcode" } = props;
     const { HiddenValues } = Catpow.Components;
-    const refs = [useRef(), useRef()];
+    const ref0 = useRef();
+    const ref1 = useRef();
+    const refs = useMemo(() => [ref0, ref1], [ref0, ref1]);
     const [value, setValue] = useState(props.value || "-");
     const [isComposing, setIsComposing] = useState(false);
     const secs = value.split("-").slice(0, 2);
-    const setSec = useCallback((i, val, isComposing2) => {
+    const setSec = useCallback((i, val) => {
       if (!val.match(/^[\d\-]+$/)) {
         val = "";
       }
@@ -19,41 +21,51 @@
       } else {
         secs[i] = val;
         if (i == 0 && val.length > 2) {
-          if (!isComposing2) {
-            refs[1].current.focus();
-          }
+          refs[1].current.focus();
         }
       }
       setValue(secs.join("-"));
-    }, []);
+    }, [refs]);
     useEffect(() => {
       if (void 0 === window.AjaxZip3) {
         return;
       }
       window.AjaxZip3.zip2addr(refs[0].current, refs[1].current, props.pref, props.addr);
-    }, [value]);
-    const input = useCallback((i) => /* @__PURE__ */ React.createElement(
-      "input",
-      {
-        type: "text",
-        size: ["3", "4"][i],
-        className: className + "__sec" + i,
-        onChange: (e) => {
-          var val = e.target.value;
-          setSec(i, e.target.value, isComposing);
-        },
-        onCompositionStart: (e) => {
-          setIsComposing(true);
-        },
-        onCompositionEnd: (e) => {
-          setSec(i, e.target.value, isComposing);
-          setIsComposing(false);
-        },
-        ref: refs[i],
-        value: secs[i]
-      }
-    ), [className, setSec, setIsComposing]);
-    return /* @__PURE__ */ React.createElement("div", { className }, input(0), /* @__PURE__ */ React.createElement("span", { className: className + "__sep" }, "-"), input(1), value && value !== "-" && /* @__PURE__ */ React.createElement(
+    }, [refs, value]);
+    const Input = useCallback((props2) => {
+      const { className: className2, index, refs: refs2 } = props2;
+      const [value2, setValue2] = useState(props2.value);
+      const [isComposing2, setIsComposing2] = useState(false);
+      useEffect(() => {
+        if (!isComposing2) {
+          setSec(index, value2);
+        }
+      }, [isComposing2, index, value2]);
+      useEffect(() => {
+        setValue2(props2.value);
+      }, [props2.value]);
+      return /* @__PURE__ */ React.createElement(
+        "input",
+        {
+          type: "text",
+          size: ["3", "4"][index],
+          className: className2,
+          onChange: (e) => {
+            setValue2(e.target.value);
+          },
+          onCompositionStart: (e) => {
+            setIsComposing2(true);
+          },
+          onCompositionEnd: (e) => {
+            setIsComposing2(false);
+            setValue2(e.target.value);
+          },
+          ref: refs2[index],
+          value: value2
+        }
+      );
+    }, [setSec]);
+    return /* @__PURE__ */ React.createElement("div", { className }, /* @__PURE__ */ React.createElement(Input, { className: className + "__sec0", index: 0, value: secs[0], refs }), /* @__PURE__ */ React.createElement("span", { className: className + "__sep" }, "-"), /* @__PURE__ */ React.createElement(Input, { className: className + "__sec1", index: 1, value: secs[1], refs }), value && value !== "-" && /* @__PURE__ */ React.createElement(
       HiddenValues,
       {
         name: props.name,
--- ui/ZipCode/component.jsx
+++ ui/ZipCode/component.jsx
@@ -1,13 +1,15 @@
 ﻿Catpow.UI.ZipCode=(props)=>{
-	const {useCallback,useState,useRef,useEffect}=React;
+	const {useCallback,useMemo,useState,useRef,useEffect}=React;
 	const {className="cmf-ui-zipcode"}=props;
 	const {HiddenValues}=Catpow.Components;
-	const refs=[useRef(),useRef()];
+	const ref0=useRef();
+	const ref1=useRef();
+	const refs=useMemo(()=>[ref0,ref1],[ref0,ref1]);
 	const [value,setValue]=useState(props.value || '-');
 	const [isComposing,setIsComposing]=useState(false);
 	const secs=value.split('-').slice(0,2);
 
-	const setSec=useCallback((i,val,isComposing)=>{
+	const setSec=useCallback((i,val)=>{
 		if(!val.match(/^[\d\-]+$/)){val='';}
 		const matches=val.match(/^(\d{3})\-?(\d{4})$/);
 		if(matches){
@@ -17,44 +19,52 @@
 		else{
 			secs[i]=val;
 			if(i==0 && val.length>2){
-				if(!isComposing){
-					refs[1].current.focus();
-				}
+				refs[1].current.focus();
 			}
 		}
 		setValue(secs.join('-'));
-	},[]);
+	},[refs]);
 	useEffect(()=>{
 		if(undefined === window.AjaxZip3){return;}
 		window.AjaxZip3.zip2addr(refs[0].current,refs[1].current,props.pref,props.addr);
-	},[value]);
+	},[refs,value]);
 	
-	const input=useCallback((i)=>(
-		<input
-			type="text"
-			size={["3","4"][i]}
-			className={className+"__sec"+i}
-			onChange={(e)=>{
-				var val=e.target.value;
-				setSec(i,e.target.value,isComposing);
-			}}
-			onCompositionStart={(e)=>{
-				setIsComposing(true);
-			}}
-			onCompositionEnd={(e)=>{
-				setSec(i,e.target.value,isComposing);
-				setIsComposing(false);
-			}}
-			ref={refs[i]}
-			value={secs[i]}
-		/>
-	),[className,setSec,setIsComposing]);
+	const Input=useCallback((props)=>{
+		const {className,index,refs}=props;
+		const [value,setValue]=useState(props.value);
+		const [isComposing,setIsComposing]=useState(false);
+		useEffect(()=>{
+			if(!isComposing){setSec(index,value);}
+		},[isComposing,index,value]);
+		useEffect(()=>{
+			setValue(props.value);
+		},[props.value]);
+		return (
+			<input
+				type="text"
+				size={["3","4"][index]}
+				className={className}
+				onChange={(e)=>{
+					setValue(e.target.value);
+				}}
+				onCompositionStart={(e)=>{
+					setIsComposing(true);
+				}}
+				onCompositionEnd={(e)=>{
+					setIsComposing(false);
+					setValue(e.target.value);
+				}}
+				ref={refs[index]}
+				value={value}
+			/>
+		);
+	},[setSec]);
 
 	return (
 		<div className={className}>
-			{input(0)}
+			<Input className={className+"__sec0"} index={0} value={secs[0]} refs={refs}/>
 			<span className={className+"__sep"}>-</span>
-			{input(1)}
+			<Input className={className+"__sec1"} index={1} value={secs[1]} refs={refs}/>
 			{value && value!=='-' && (
 				<HiddenValues
 					name={props.name}
