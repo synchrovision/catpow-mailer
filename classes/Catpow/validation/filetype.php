cd6c6d13c5e3f1de22626895dd26549395c2411f
synchrovision <hatano@catpow.info>
2024-09-07 19:32:16 +0900

    feat :+1: update filetype validation class

--- classes/Catpow/validation/filetype.php
+++ classes/Catpow/validation/filetype.php
@@ -6,13 +6,21 @@ class filetype extends validation{
 	public static function is_valid(&$val,$input){
 		$accept=isset(static::$accept)?static::$accept:$input->conf['accept'];
 		if(empty($accept)){return true;}
-		$fileMimeType=mime_content_type($val['tmp_name']);
 		if(is_string($accept)){$accept=explode(',',$accept);}
 		foreach($accept as $i=>$mime){
 			if($mime[0]==='.'){$accept[$i]=$mime=MimeType::ext_to_mime($mime);}
 			if(MimeType::test_filetype($val,$mime)){return true;}
 		}
 		return false;
+	}
+	public static function get_karma($val,$input){
+		list($fileType,$fileSubtype)=explode('/',$val['type']);
+		if($fileType==='application'){
+			$accept=isset(static::$accept)?static::$accept:$input->conf['accept'];
+			if(is_string($accept)){$accept=explode(',',$accept);}
+			if(!in_array($val['type'],$accept)){return 'danger_file_upload';}
+		}
+		return 0;
 	}
 	public static function get_message_format($conf){
 		return __('許可されないファイル形式です');
