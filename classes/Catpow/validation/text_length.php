9431896f208b47dfe90ff240d785b9195d3115b5
synchrovision <hatano@catpow.info>
2024-09-07 19:55:27 +0900

    fix :bug:

--- classes/Catpow/validation/text_length.php
+++ classes/Catpow/validation/text_length.php
@@ -1,11 +1,11 @@
-meta<?php
+<?php
 namespace Catpow\validation;
 
 class text_length extends validation{
 	public static $message_keys=array('length');
 	
 	public static function is_valid(&$val,$input){
-		return mb_strlen($val)!==(int)$input->conf['length'];
+		return mb_strlen($val)===(int)$input->conf['length'];
 	}
 	public static function get_message_format($conf){
 		return __('%d文字で入力してください');
