4d331dd0de759d3947ab99e9ca15e8c4e8409479
synchrovision <trinitybluster@yahoo.co.jp>
2024-09-12 01:09:03 +0900

    feat :+1: implement MailMediaText component

--- classes/Component/MailMediaText.php
+++ classes/Component/MailMediaText.php
@@ -1,4 +1,22 @@
 <?php
 namespace Catpow\Component;
 class MailMediaText extends Component{
+	public static function translate($el,$doc){
+		$children=[];
+		$level=self::getLevel($el);
+		foreach($el->childNodes as $child){
+			if(!is_a($child,\DOMElement::class)){continue;}
+			if($child->tagName==='media'){
+				$children[]=['td',['class'=>$el->tagName.'__tbody-tr-td is-media'],[$child]];
+			}
+			elseif($child->tagName==='text'){
+				$children[]=['td',['class'=>$el->tagName.'__tbody-tr-td is-text'],[$child]];
+			}
+		}
+		return self::arrayToNode(['table',['class'=>$el->tagName.' is-level-'.$level,'level'=>$level],[
+			['tbody',['class'=>$el->tagName.'__tbody'],[
+				['tr',['class'=>$el->tagName.'__tbody-tr'],$children]
+			]]
+		]],$doc);
+	}
 }
