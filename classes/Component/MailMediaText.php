8545ad74c10c619546b712b5fc2a2081cecb7955
synchrovision <hatano@catpow.info>
2024-09-15 19:58:01 +0900

    feat :+1: update mail-mediatext component

--- classes/Component/MailMediaText.php
+++ classes/Component/MailMediaText.php
@@ -1,21 +1,21 @@
 <?php
 namespace Catpow\Component;
-class MailMediaText extends Component{
+class MailMediatext extends Component{
 	public static function translate($el,$doc){
 		$children=[];
 		$level=self::getLevel($el);
 		foreach($el->childNodes as $child){
 			if(!is_a($child,\DOMElement::class)){continue;}
 			if($child->tagName==='media'){
-				$children[]=['td',['class'=>$el->tagName.'__tbody-tr-td is-media'],[$child]];
+				$children[]=['td',['class'=>$el->tagName.'-media'],[$child]];
 			}
 			elseif($child->tagName==='text'){
-				$children[]=['td',['class'=>$el->tagName.'__tbody-tr-td is-text'],[$child]];
+				$children[]=['td',['class'=>$el->tagName.'-text'],[$child]];
 			}
 		}
-		return self::arrayToNode(['table',['class'=>$el->tagName.' is-level-'.$level,'level'=>$level],[
+		return self::arrayToNode(['table',['class'=>$el->tagName.' is-level-'.$level],[
 			['tbody',['class'=>$el->tagName.'__tbody'],[
-				['tr',['class'=>$el->tagName.'__tbody-tr'],$children]
+				['tr',['class'=>$el->tagName.'-item'],$children]
 			]]
 		]],$doc);
 	}
