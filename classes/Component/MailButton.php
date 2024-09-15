c1b9bde756c433c630ab4310361191e43421f07d
synchrovision <hatano@catpow.info>
2024-09-15 19:58:15 +0900

    feat :+1: update mail-button component

--- classes/Component/MailButton.php
+++ classes/Component/MailButton.php
@@ -2,11 +2,12 @@
 namespace Catpow\Component;
 class MailButton extends Component{
 	public static function translate($el,$doc){
-		return self::arrayToNode(['table',['class'=>$el->tagName],[
+		$level=self::getLevel($el);
+		return self::arrayToNode(['table',['class'=>$el->tagName.' is-level-'.$level],[
 			['tbody',['class'=>$el->tagName.'__tbody'],[
 				['tr',['class'=>$el->tagName.'__tbody-tr'],[
-					['td',['class'=>$el->tagName.'__tbody-tr-td'],[
-						['a',['class'=>$el->tagName.'-button','href'=>$el->getAttribute('href')],[$el->childNodes]]
+					['td',['class'=>$el->tagName.'-item is-level-'.$level],[
+						['a',['class'=>$el->tagName.'-item-button','href'=>$el->getAttribute('href')],[$el->childNodes]]
 					]]
 				]]
 			]]
